dd98ac884fc15480a01e72d634aee2ee7b632a1a
VelmaAkoch <velmaakochi1@gmail.com>
2022-03-17 16:52:12 +0300

    all the files and changes done

---
+++ script.js
@@ -0,0 +1,55 @@
+$(".design").click(function(){
+    $(".design").toggle()
+    $(".design2").toggle()
+})
+
+$(".development").click(function(){
+    $(".development").toggle()
+    $(".development2").toggle()
+})
+
+$(".product").click(function(){
+    $(".product").toggle()
+    $(".product2").toggle()
+
+})
+
+$(".workone").hover(function(){
+    $(".image1-1").toggle()
+    $(".workone").toggle()
+})
+
+$(".worktwo").hover(function(){
+    $(".image2-1").toggle()
+    $(".worktwo").toggle()
+})
+
+$(".workthree").hover(function(){
+    $(".image3-1").toggle()
+    $(".workthree").toggle()
+})
+
+$(".workfour").hover(function(){
+    $(".image4-1").toggle()
+    $(".workfour").toggle()
+})
+
+$(".workfive").hover(function(){
+    $(".image5-1").toggle()
+    $(".workfive").toggle()
+})
+
+$(".worksix").hover(function(){
+    $(".image6-1").toggle()
+    $(".worksix").toggle()
+})
+
+$(".workseven").hover(function(){
+    $(".image7-1").toggle()
+    $(".workseven").toggle()
+})
+
+$(".workeight").hover(function(){
+    $(".image8-1").toggle()
+    $(".workeight").toggle()
+})
